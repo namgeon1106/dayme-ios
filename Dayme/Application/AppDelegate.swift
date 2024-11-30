ee5aaf423ceb606645f1312cc03c38e1d3914070
eastlooo <nupic7@gmail.com>
2024-11-30 23:52:48 +0900

    feat: FCM 등록

--- Dayme/Application/AppDelegate.swift
+++ Dayme/Application/AppDelegate.swift
@@ -11,9 +11,16 @@ import FirebaseCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    private let noticationService: NotificationService = .shared
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        noticationService.configure()
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        noticationService.registerDeviceToken(deviceToken)
     }
     
 }
---
+++ Dayme/Service/Notification/NotificationService.swift
@@ -0,0 +1,69 @@
+//
+//  NotificationService.swift
+//  Dayme
+//
+//  Created by 정동천 on 11/30/24.
+//
+
+import UIKit
+import FirebaseMessaging
+import UserNotifications
+
+final class NotificationService: NSObject {
+    
+    static let shared = NotificationService()
+    
+    private var messaging: Messaging!
+    private var notiCenter: UNUserNotificationCenter!
+    
+    
+    func configure() {
+        messaging = .messaging()
+        notiCenter = .current()
+        messaging.delegate = self
+        notiCenter.delegate = self
+    }
+    
+    @MainActor
+    @discardableResult
+    func requestAuthorization() async throws -> Bool {
+        let options: UNAuthorizationOptions = [.sound, .alert, .badge]
+        let granted = try await notiCenter.requestAuthorization(options: options)
+        
+        if granted {
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+        
+        return granted
+    }
+    
+    func registerDeviceToken(_ deviceToken: Data) {
+        messaging.apnsToken = deviceToken
+    }
+    
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+
+extension NotificationService: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        let userInfo = response.notification.request.content.userInfo as NSDictionary
+        Logger.debug { "\(userInfo)" }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        return [.sound, .banner, .list]
+    }
+    
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+
+extension NotificationService: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        Logger.debug { "FCM TOKEN RECEIVED\n\(fcmToken.orEmpty)" }
+    }
+    
+}
