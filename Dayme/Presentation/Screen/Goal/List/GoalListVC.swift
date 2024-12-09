59e10602943f2f74dff42f7a69862e2a70619e6c
eastlooo <nupic7@gmail.com>
2024-12-09 22:29:08 +0900

    feat: 주요목표 리스트 화면 섹션분리

--- Dayme/Presentation/Screen/Goal/List/GoalListVC.swift
+++ Dayme/Presentation/Screen/Goal/List/GoalListVC.swift
@@ -23,11 +23,16 @@ final class GoalListVC: UITableViewController {
     
     var goals: [Goal] = [] {
         didSet {
+            homeGoals = goals.filter { $0.displayHome }
+            etcGoals = goals.filter { !$0.displayHome }
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
     }
+    
+    private var homeGoals: [Goal] = []
+    private var etcGoals: [Goal] = []
     
     
     init() {
@@ -49,6 +54,7 @@ final class GoalListVC: UITableViewController {
         tableView.tableHeaderView?.frame.size.height = 24
         tableView.tableFooterView = UIView()
         tableView.tableFooterView?.frame.size.height = 24
+        tableView.sectionHeaderTopPadding = 0
     }
     
 }
@@ -57,15 +63,74 @@ final class GoalListVC: UITableViewController {
 
 extension GoalListVC {
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        goals.count
+        section == 0 ? homeGoals.count : etcGoals.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let goal = indexPath.section == 0 ? homeGoals[indexPath.row] : etcGoals[indexPath.row]
         let cell: GoalListCell = tableView.dequeueReusableCell(for: indexPath)
-        cell.bind(goals[indexPath.row])
+        cell.bind(goal)
         cell.delegate = cellDelegate
         return cell
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+
+extension GoalListVC {
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if homeGoals.isEmpty {
+            return nil
+        }
+        
+        let container = UIView()
+        
+        if section == 0 {
+            let label = UILabel("홈화면 표시")
+                .textColor(.colorGrey50)
+                .font(.pretendard(.bold, 14))
+            label.translatesAutoresizingMaskIntoConstraints = false
+            container.addSubview(label)
+            NSLayoutConstraint.activate([
+                label.topAnchor.constraint(equalTo: container.topAnchor),
+                label.leftAnchor.constraint(equalTo: container.leftAnchor, constant: 24),
+            ])
+        } else {
+            if etcGoals.isEmpty {
+                return nil
+            }
+            
+            let separator = UIView().backgroundColor(.colorGrey20)
+            separator.translatesAutoresizingMaskIntoConstraints = false
+            container.addSubview(separator)
+            NSLayoutConstraint.activate([
+                separator.leftAnchor.constraint(equalTo: container.leftAnchor, constant: 24),
+                separator.rightAnchor.constraint(equalTo: container.rightAnchor, constant: -24),
+                separator.centerYAnchor.constraint(equalTo: container.centerYAnchor),
+                separator.heightAnchor.constraint(equalToConstant: 2),
+            ])
+        }
+        
+        return container
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if homeGoals.isEmpty {
+            return 0
+        }
+        
+        if section == 0 {
+            return 24
+        } else {
+            return etcGoals.isEmpty ? 0 : 32
+        }
     }
     
 }
